77032d5b3f97a7e7e15e0a440c9505d626794131
Carlos Eduardo <kaduu5050@outlook.com>
2022-04-21 23:45:09 -0300

    now reconnect after "econnreset"

--- comandos/play.js
+++ comandos/play.js
@@ -7,6 +7,8 @@ const {
 	createAudioResource,
 	joinVoiceChannel,
 } = require('@discordjs/voice');
+const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path
+const fluentffmpeg = require('fluent-ffmpeg')
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -18,6 +20,8 @@ module.exports = {
                 .setRequired(true)
         ),
     async execute(interaction) {
+
+        fluentffmpeg.setFfmpegPath(ffmpegPath)
 
         if (interaction.member.voice.channel) {
             var connection = joinVoiceChannel({
@@ -31,18 +35,42 @@ module.exports = {
         }
 
         url = interaction.options.getString('link')
-        const stream = ytdl(url, { filter: 'audioonly'})
+        const stream = ytdl(url, { quality: 'lowestaudio', filter: form => {
+            if (form.bitrate && interaction.member.voice.channel?.bitrate) return form.bitrate <= interaction.member.voice.channel.bitrate;
+            return false
+        }})
         const resource = createAudioResource(stream, { inputType: StreamType.Arbitrary })
         const player = createAudioPlayer()
 
         player.play(resource)
         connection.subscribe(player)
+        
+        const tempo = {}
+        player.on(AudioPlayerStatus.Playing, () => {
+            console.log('Contando tempo de execução...')
+            tempo['start'] = Math.round(performance.now())
+
+            ytdl.getInfo(url).then(info => {
+                interaction.editReply(`Conectado em ${interaction.member.voice.channel}\n\nTocando agora: ${info.videoDetails.title}`)
+            })
+        })
+        
+        player.on('error', error => {
+            tempo['end'] = Math.round(performance.now())
+            console.error(`Ocorreu um erro: ${error} - Tentando reconectar...`)
+            let elapsed = tempo['end'] - tempo['start']
+
+            const song = ytdl(url, { quality: 'lowestaudio', highWaterMark: 64, filter: form => {
+                if (form.bitrate && interaction.member.voice.channel?.bitrate) return form.bitrate <= interaction.member.voice.channel.bitrate;
+                return false
+            }})
+            const retry = fluentffmpeg({source: song}).toFormat('mp3').setStartTime(Math.ceil(elapsed/1000))
+            const resourceretry = createAudioResource(retry, { inputType: StreamType.Arbitrary })
+            
+            player.play(resourceretry)
+        })
 
         player.on(AudioPlayerStatus.Idle, () => connection.destroy())
 
-        interaction.client.on('error', error => {
-            console.error(`Ocorreu um erro: ${error}`)
-            console.log(`Ocorreu um erro: ${error}`)
-        })
     },
 };
