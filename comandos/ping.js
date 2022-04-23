9405a99c3af06c1a6610b65bac2cc0feeda149ef
Carlos Eduardo <kaduu5050@outlook.com>
2022-04-23 00:59:09 -0300

    remover o chromedriver do repositorio

--- comandos/ping.js
+++ comandos/ping.js
@@ -1,13 +1,10 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
-const wait = require('node:timers/promises').setTimeout;
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('ping')
         .setDescription('Responde com pong'),
     async execute(interaction) {
-        await interaction.deferReply();
-        await wait(2000);
-        await interaction.editReply('pong carai')
+        await interaction.reply('pong')
     },
 };
